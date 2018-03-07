e2e614ad465402565b7abc0b04f8c43bd5fe0c51
Matt Hoyle <code@deployable.co>
2018-03-07 07:02:11 +0000

    improve coverage

--- test/unit/DomainName.js
+++ test/unit/DomainName.js
@@ -1,15 +1,46 @@
 /* global expect */
 const DomainName = require('../../lib/DomainName')
 
-describe('Unit', function(){
+describe('Unit::DomainName', function(){
 
-  describe('DomainName', function(){
+  it('should create a DomainName', function(){
+    let node = new DomainName('one')
+    expect( node.id ).to.equal( 'one' )
+  })
 
-    it('root', function(){
-      let node = new DomainName('one')
-      expect( node.id ).to.equal( 'one' )
+  it('should convert a root domain to JSON', function(){
+    let node = new DomainName('one')
+    expect( node.toJSON() ).to.eql({
+      domain: '',
     })
+  })
 
+  it('should convert a root domain to JSON', function(){
+    let node = new DomainName('one', {})
+    expect( node.domainComponents() ).to.eql([ 'one' ])
+  })
+
+  it('should convert a root domain to JSON', function(){
+    let root1 = { level: 0 }
+    let node1 = new DomainName('one', root1)
+    let node2 = new DomainName('two', node1)
+    expect( node2.domainComponents() ).to.eql([ 'one', 'two' ])
+  })
+
+  it('should convert a non root domain to JSON', function(){
+    let node = new DomainName('one', {})
+    expect( node.toJSON() ).to.eql({
+      domain: 'one',
+    })
+  })
+
+  it('should convert a non root domain with data to JSON', function(){
+    let node = new DomainName('one', {})
+    node.setData({ one: 1 })
+    expect( node.toJSON() ).to.eql({
+      _data: { one: 1 },
+      domain: 'one',
+    })
   })
 
 })
--- test/unit/Tree.js
+++ test/unit/Tree.js
@@ -2,40 +2,36 @@
 const Tree = require('../../lib/Tree')
 
 
-describe('Unit', function(){
+describe('Unit::Tree', function(){
 
-  describe('Tree', function(){
+  it('should get a node', function(){
+    let tree = new Tree()
+    let node = tree.addNode('first')
+    expect( tree.getNode('first') ).to.be.ok
+  })
 
-    it('should get a node', function(){
-      let tree = new Tree()
-      let node = tree.addNode('first')
-      expect( tree.getNode('first') ).to.be.ok
+  it('should get a sub node', function(){
+    let tree = new Tree()
+    let nodef = tree.addNode('first')
+    let nodes = nodef.addNode('second')
+    expect( tree.getNode('first') ).to.be.ok
+    expect( nodef.getNode('second') ).to.be.ok
+    expect( tree.fetchNode('first.second') ).to.be.ok
+  })
+
+  it('should convert toJson', function(){
+    let tree = new Tree()
+    let one = tree.addNode('one')
+    let two = tree.addNode('two')
+    one.replaceData({ one: 1 })
+    two.setData({ two: 2 })
+    two.setData({ thr: 3 })
+    expect( tree.toJSON() ).to.eql({
+      children: [
+        { id: 'one', _data: { one: 1 }, children: [] },
+        { id: 'two', _data: { two: 2, thr: 3 }, children: [] }
+      ]
     })
-
-    it('should get a sub node', function(){
-      let tree = new Tree()
-      let nodef = tree.addNode('first')
-      let nodes = nodef.addNode('second')
-      expect( tree.getNode('first') ).to.be.ok
-      expect( nodef.getNode('second') ).to.be.ok
-      expect( tree.fetchNode('first.second') ).to.be.ok
-    })
-
-    it('should convert toJson', function(){
-      let tree = new Tree()
-      let one = tree.addNode('one')
-      let two = tree.addNode('two')
-      one.replaceData({ one: 1 })
-      two.setData({ two: 2 })
-      two.setData({ thr: 3 })
-      expect( tree.toJSON() ).to.eql({
-        children: [
-          { id: 'one', _data: { one: 1 }, children: [] },
-          { id: 'two', _data: { two: 2, thr: 3 }, children: [] }
-        ]
-      })
-    })
-
   })
 
 })
