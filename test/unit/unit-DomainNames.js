66ab3b75b1a2f9c262c9768488e87b5168bc7124
Matt Hoyle <code@deployable.co>
2018-03-07 02:17:16 +0000

    fix test bug from linting changes

--- test/unit/unit-DomainNames.js
+++ test/unit/unit-DomainNames.js
@@ -7,7 +7,7 @@ describe('Unit::DomainNames', function(){
   let tree = null
 
   beforeEach(function(){
-    let tree = new DomainNames()
+    tree = new DomainNames()
     tree.addDomain('test.whatever.com', { test: true })
     tree.addDomain('two.whatever.com', {two: true })
     tree.addDomain('other.com', { other: true })
